6d360929965bbdaee2635eeed7d3185781cf60f7
Alexander Taylor <alexanderjohntaylor@gmail.com>
2016-08-07 21:49:27 +0100

    Added android build stuff to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,23 @@ from Cython.Distutils import build_ext
 
 packages = find_packages()
 
+options = {'apk': {'debug': None,
+                   'requirements': 'sdl2,kivy',
+                   'android-api': 19,
+                   'ndk-dir': '/home/asandy/android/crystax-ndk-10.3.1',
+                   'dist-name': 'pyde',
+                   'ndk-version': '10.3.1',
+                   'package': 'net.inclem.pyde',
+                   'service': 'interpreter:interpreter_subprocess/interpreter.py'
+                   }}
 setup(
     name='PyDE',
     description='A Python mobile IDE experiment',
     author='Alexander Taylor',
     author_email='alexanderjohntaylor@gmail.com',
     packages=packages,
+    options=options,
+    package_data={'pyde': ['*.py', '*.kv'],
+                  'pyde/interpreter_subprocess', ['*.py'],
+                  'pyde/assets', ['*.ttf', '*.txt']}
 )
