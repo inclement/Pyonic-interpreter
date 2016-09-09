f18e13a877deb3b86289dd1ad3e891cf1b05ccb5
Alexander Taylor <alexanderjohntaylor@gmail.com>
2016-09-09 23:08:33 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -5,18 +5,20 @@ from Cython.Distutils import build_ext
 packages = find_packages()
 
 options = {'apk': {'debug': None,
-                   'requirements': 'sdl2,kivy',
+                   'requirements': 'sdl2,kivy,python2',
                    'android-api': 19,
-                   'ndk-dir': '/home/asandy/android/crystax-ndk-10.3.1',
-                   'dist-name': 'pyde',
+                   'ndk-dir': '/home/asandy/android/crystax-ndk-10.3.2',
+                   # 'dist-name': 'pyde',
+                   'dist-name': 'pyde_new',
                    'ndk-version': '10.3.1',
                    'package': 'net.inclem.pyde',
                    'permission': 'INTERNET',
-                   'service': 'interpreter:interpreter_subprocess/interpreter.py'
+                   'service': 'interpreter:interpreter_subprocess/interpreter.py',
+                   'arch': 'armeabi-v7a',
                    }}
 setup(
     name='PyDE',
-    version='0.1',
+    version='0.2',
     description='A Python mobile IDE experiment',
     author='Alexander Taylor',
     author_email='alexanderjohntaylor@gmail.com',
