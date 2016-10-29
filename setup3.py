ba5487f8cbc486392076b19a53140b810b115097
Alexander Taylor <alexanderjohntaylor@gmail.com>
2016-10-29 21:43:49 +0100

    Updated setup.py structure for py2/py3 versions

--- setup3.py
+++ setup3.py
@@ -5,22 +5,22 @@ from Cython.Distutils import build_ext
 packages = find_packages()
 
 options = {'apk': {'window': None,
-                   'requirements': 'sdl2,kivy,python2,pygments',
+                   'requirements': 'sdl2,kivy,python3crystax,pygments',
                    'android-api': 19,
                    'ndk-dir': '/home/asandy/android/crystax-ndk-10.3.2',
-                   'dist-name': 'pyonic_python2',
+                   'dist-name': 'pyonic',
                    'ndk-version': '10.3.2',
-                   'package': 'net.inclem.pyonicinterpreter',
+                   'package': 'net.inclem.pyonicinterpreter3',
                    'permission': 'INTERNET',
                    'service': 'interpreter:interpreter_subprocess/interpreter.py',
                    'arch': 'armeabi-v7a',
-                   'icon': 'build_assets/icon_py2-96.png',
+                   'icon': 'build_assets/icon_py3-96.png',
                    'presplash': 'build_assets/presplash.png',
                    # 'release': None,
                    #'debug': None,
                    }}
 setup(
-    name='Pyonic Python 2 interpreter',
+    name='Pyonic Python 3 interpreter',
     version='0.7',
     description='A Python mobile IDE experiment',
     author='Alexander Taylor',
