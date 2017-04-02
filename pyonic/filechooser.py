fddd59ad7a99d7aee5224e4add8f670b4f40c214
Alexander Taylor <alexanderjohntaylor@gmail.com>
2017-04-02 15:27:25 +0100

    More filechooser improvements

--- pyonic/filechooser.py
+++ pyonic/filechooser.py
@@ -3,38 +3,60 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
 from kivy.uix.recycleview import RecycleView
+from kivy.uix.behaviors import ButtonBehavior
 from kivy.properties import (StringProperty, OptionProperty,
-                             BooleanProperty, NumericProperty)
+                             BooleanProperty, NumericProperty,
+                             ObjectProperty)
 from kivy.lang import Builder
 
 import os
-from os.path import isdir, join
+from os.path import isdir, join, abspath
 
 Builder.load_file('filechooser.kv')
 
-class FileLabel(BoxLayout):
+class FileLabel(ButtonBehavior, BoxLayout):
     file_type = OptionProperty('file', options=('folder', 'file'))
     filename = StringProperty()
     shade = BooleanProperty(False)
+    selected = BooleanProperty(False)
+    
+    @property
+    def recycleview(self):
+        return self.parent.parent
+
+    def on_release(self):
+        self.selected = not self.selected
+        if self.selected:
+            self.recycleview.select(self)
+            if self.file_type == 'folder':
+                self.recycleview.folder = abspath(
+                    join(self.recycleview.folder, self.filename))
+        else:
+            self.recycleview.select(None)
 
 class FileView(RecycleView):
     folder = StringProperty('.')
     python_only = BooleanProperty(False)
+    selection_instance = ObjectProperty(allownone=True)
+    selection_filename = StringProperty()
+
 
     def __init__(self, *args, **kwargs):
         super(FileView, self).__init__(*args, **kwargs)
+        self.on_folder(self, self.folder)
+
+    def on_python_only(self, instance, value):
         self.on_folder(self, self.folder)
 
     def on_folder(self, instance, value):
         filens = os.listdir(self.folder)
         filens.append('..')
 
-        if self.python_only:
-            filens = [filen for filen in filens if filen.endswith('.py')]
-
         file_types = ['folder' if isdir(join(self.folder, filen)) else 'file' for filen in filens]
 
         files = zip(filens, file_types)
+        if self.python_only:
+            files = [filen for filen in files if filen[0].endswith('.py') or filen[1] == 'folder']
         files = sorted(files, key=lambda row: row[0].lower())
         files = sorted(files, key=lambda row: (row[1] != 'folder'))
 
@@ -48,6 +70,19 @@ class FileView(RecycleView):
                      for (name, file_type, index) in files]
         print('data is', self.data)
 
+        self.reset_scroll()
+
+    def select(self, widget):
+        if self.selection_instance is not None:
+            self.selection_instance.selected = False
+        self.selection_instance = widget
+        if widget is not None:
+            self.selection_filename = widget.filename
+        else:
+            self.selection_filename = ''
+
+    def reset_scroll(self):
+        self.scroll_y = 1
 
 
 class PyonicFileChooser(BoxLayout):
